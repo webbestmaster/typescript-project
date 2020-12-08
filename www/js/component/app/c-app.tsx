2d4972a60c9d91772fc9bc1c8497ae2b83d62377
dim <web.best.master@gmail.com>
2020-12-08 13:07:53 +0300

    WIP

--- www/js/component/app/c-app.tsx
+++ www/js/component/app/c-app.tsx
@@ -3,5 +3,23 @@ import React from 'react';
 import appStyle from './app.scss';
 
 export function App(): JSX.Element {
+    const a = 1;
+
+    if (a === 1) {
+        if (a === 2) {
+            if (a === 3) {
+                if (a === 4) {
+                    if (a === 5) {
+                        [1, 2, 3, a].forEach(() => {
+                            {
+                                console.log(11);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    }
+
     return <h1 className={appStyle.app_header}>App</h1>;
 }
