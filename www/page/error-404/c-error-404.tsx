46ef0d24fe2bdbcd6d36faa545e12c152ceaa9c1
dim <turovtsovd@rocketdata.io>
2021-05-24 18:04:55 +0300

    ncu

--- www/page/error-404/c-error-404.tsx
+++ www/page/error-404/c-error-404.tsx
@@ -1,3 +1,22 @@
+import {Spinner} from '../../layout/spinner/c-spinner';
+import {IsRender} from '../../layout/is-render/c-is-render';
+import {IsHidden} from '../../layout/is-hidden/c-is-hidden';
+import {Empty} from '../../layout/empty/c-empty';
+
 export function Error404(): JSX.Element {
-    return <h1>Page 404</h1>;
+    return (
+        <div>
+            <h1>Page 404</h1>
+
+            <IsRender isRender={false}>
+                <IsHidden isHidden>
+                    <div>
+                        <Empty />
+
+                        <Spinner />
+                    </div>
+                </IsHidden>
+            </IsRender>
+        </div>
+    );
 }
