137c2c1bb98b5f34a8a0d4b40b00a61542661390
dim <web.best.master@gmail.com>
2021-09-21 22:21:08 +0300

    update build library script

--- dist/library.d.ts
+++ dist/library.d.ts
@@ -1,1 +1,9 @@
 export { LibraryComponent as Library } from './library-component/library-component';
+
+declare module 'typescript-project/dist/style.css' {
+    type StyleType = Record<string, string>;
+
+    const style: StyleType;
+
+    export default style;
+}
