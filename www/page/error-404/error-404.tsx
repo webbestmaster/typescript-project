584bf39377a83822e881647bcc6c1e0ad09a6358
dim <web.best.master@gmail.com>
2022-02-16 21:57:13 +0100

    update

--- www/page/error-404/error-404.tsx
+++ www/page/error-404/error-404.tsx
@@ -3,10 +3,14 @@ import {IsRender} from '../../layout/is-render/is-render';
 import {IsHidden} from '../../layout/is-hidden/is-hidden';
 import {Empty} from '../../layout/empty/empty';
 
+import page404Style from './page-404.scss';
+
 export function Error404(): JSX.Element {
     return (
         <div>
-            <h1>Page 404</h1>
+            <h1 className={page404Style.page_404_header} data-text="Page 404">
+                Page 404
+            </h1>
 
             <IsRender isRender={false}>
                 <IsHidden isHidden={false}>
