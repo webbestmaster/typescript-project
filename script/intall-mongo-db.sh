2375da97fde03bbf3f84cbe3c02f021d5495ab70
Dmitry Turovtsov <web.best.master@gmail.com>
2022-03-10 22:39:00 +0100

    Add files via upload

---
+++ script/intall-mongo-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ~ || return
+
+sudo apt update
+
+#sudo apt install curl
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+sudo apt update
+
+sudo apt install -y mongodb-org
+
+
--- script/intall-node-js.sh
+++ script/intall-node-js.sh
@@ -4,12 +4,14 @@ cd ~ || return
 
 sudo apt update
 
-sudo apt install curl
+#sudo apt install curl
 
 curl -sL https://deb.nodesource.com/setup_17.x -o ~/nodesource_setup.sh
 
 sudo bash ~/nodesource_setup.sh
 
 sudo apt-get install -y nodejs
+
+sudo apt update
 
 sudo npm install -g npm
