e6cc696d10c7b4804e41185083052857eb7fa956
dim <web.best.master@gmail.com>
2022-03-19 21:19:43 +0100

    update

--- server/article/article-type.ts
+++ server/article/article-type.ts
@@ -12,26 +12,26 @@ export enum SubDocumentListViewTypeEnum {
 
 export type ArticleType = {
     articleType: ArticleTypeEnum;
-    artistList: Array<string>;
-    authorList: Array<string>;
-    compositorList: Array<string>;
     content: string;
     createdDate: string;
     description: string;
-    directorList: Array<string>;
     fileList: Array<string>;
     hasMetaRobotsFollowSeo: boolean; // Add/combine <meta name="robots" content="nofollow"/>
     hasMetaRobotsNoIndexSeo: boolean; // Add/combine <meta name="robots" content="noindex"/> and add X-Robots-Tag: noindex
     id: string;
-    illustratorList: Array<string>;
     isActive: boolean; // actually temporary "removed"
     isInSiteMapXmlSeo: boolean; // has sitemap.xml link to article on not
     metaDescriptionSeo: string; // tag <meta type="description" content="....." />
     metaSeo: string; // actually any html code
     publishDate: string;
-    readerList: Array<string>;
     shortDescription: string;
     slug: string;
+    stuffArtistList: Array<string>;
+    stuffAuthorList: Array<string>;
+    stuffCompositorList: Array<string>;
+    stuffDirectorList: Array<string>;
+    stuffIllustratorList: Array<string>;
+    stuffReaderList: Array<string>;
     subDocumentIdList: Array<string>;
     subDocumentListViewType: SubDocumentListViewTypeEnum;
     tagList: Array<string>;
