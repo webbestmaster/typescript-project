f783ec645dddc602d28db7d633e24d114af5c978
dim <web.best.master@gmail.com>
2022-05-27 21:25:26 +0200

    update

--- www/layout/markdown.tsx
+++ www/layout/markdown.tsx
@@ -1,7 +1,7 @@
 /* global HTMLDivElement */
 
 import {HTMLAttributes} from 'react';
-import {markdown} from 'markdown-pro';
+import {markdown, defaultMarkdownConfig} from 'markdown-pro';
 
 import {classNames} from '../util/css';
 
@@ -12,13 +12,12 @@ type PropsType = HTMLAttributes<HTMLDivElement> & {
 export function Markdown(props: PropsType): JSX.Element {
     const {mdInput, ...divAttributes} = props;
     const {className} = divAttributes;
-    // eslint-disable-next-line react/no-danger, id-match
 
     return (
         <div
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...divAttributes}
-            className={classNames('md-pro', className)}
+            className={classNames(defaultMarkdownConfig.wrapperClassName, className)}
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{__html: markdown(mdInput, {useWrapper: false})}}
         />
