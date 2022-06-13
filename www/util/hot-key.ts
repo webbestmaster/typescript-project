f9ed54580ba5d430113ecc563cede2a88b25ab91
Dmitry Turovtsov <web.best.master@gmail.com>
2022-06-13 15:04:09 +0200

    Add files via upload

--- www/util/hot-key.ts
+++ www/util/hot-key.ts
@@ -4,17 +4,59 @@ import {useEffect} from 'react';
 export function useCtrlShiftKey(char: string, handleHotKey: () => unknown) {
     useEffect(() => {
         function handleBodyOnKeyPress(evt: KeyboardEvent) {
-            console.log('---> handleBodyOnKeyPress', evt);
+            console.log('---> useCtrlShiftKey', evt);
 
-            if (evt.shiftKey && evt.ctrlKey && evt.code.toLowerCase() === `key${char}`.toLowerCase()) {
+            evt.preventDefault();
+
+            if (evt.ctrlKey && evt.shiftKey && evt.code.toLowerCase() === `key${char}`.toLowerCase()) {
                 handleHotKey();
             }
         }
 
-        document.body.addEventListener('keypress', handleBodyOnKeyPress, false);
+        document.body.addEventListener('keydown', handleBodyOnKeyPress, false);
 
         return () => {
-            document.body.removeEventListener('keypress', handleBodyOnKeyPress, false);
+            document.body.removeEventListener('keydown', handleBodyOnKeyPress, false);
+        };
+    }, [char, handleHotKey]);
+}
+
+export function useCtrlAltKey(char: string, handleHotKey: () => unknown) {
+    useEffect(() => {
+        function handleBodyOnKeyPress(evt: KeyboardEvent) {
+            console.log('---> useCtrlAltKey', evt);
+
+            evt.preventDefault();
+
+            if (evt.ctrlKey && evt.altKey && evt.code.toLowerCase() === `key${char}`.toLowerCase()) {
+                handleHotKey();
+            }
+        }
+
+        document.body.addEventListener('keydown', handleBodyOnKeyPress, false);
+
+        return () => {
+            document.body.removeEventListener('keydown', handleBodyOnKeyPress, false);
+        };
+    }, [char, handleHotKey]);
+}
+
+export function useShiftAltKey(char: string, handleHotKey: () => unknown) {
+    useEffect(() => {
+        function handleBodyOnKeyPress(evt: KeyboardEvent) {
+            console.log('---> useShiftAltKey', evt);
+
+            evt.preventDefault();
+
+            if (evt.shiftKey && evt.altKey && evt.code.toLowerCase() === `key${char}`.toLowerCase()) {
+                handleHotKey();
+            }
+        }
+
+        document.body.addEventListener('keydown', handleBodyOnKeyPress, false);
+
+        return () => {
+            document.body.removeEventListener('keydown', handleBodyOnKeyPress, false);
         };
     }, [char, handleHotKey]);
 }
