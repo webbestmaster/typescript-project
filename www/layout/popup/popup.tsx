3af52246fb47998147576c342d3e3203997e5a72
dim <web.best.master@gmail.com>
2022-06-25 13:35:54 +0200

    update

--- www/layout/popup/popup.tsx
+++ www/layout/popup/popup.tsx
@@ -3,32 +3,43 @@
 import {ReactNode, useEffect, useRef, useState} from 'react';
 import {createPortal} from 'react-dom';
 
+import popupStyle from './popup.scss';
+import {PopupVisibleStateEnum} from './popup-const';
+
+const fadeClassNameMap: Record<PopupVisibleStateEnum, string> = {
+    [PopupVisibleStateEnum.closed]: popupStyle.popup__fade__closed,
+    [PopupVisibleStateEnum.opening]: popupStyle.popup__fade__opening,
+    [PopupVisibleStateEnum.open]: popupStyle.popup__fade__open,
+    [PopupVisibleStateEnum.closing]: popupStyle.popup__fade__closing,
+};
+
+const containerClassNameMap: Record<PopupVisibleStateEnum, string> = {
+    [PopupVisibleStateEnum.closed]: popupStyle.popup__container__closed,
+    [PopupVisibleStateEnum.opening]: popupStyle.popup__container__opening,
+    [PopupVisibleStateEnum.open]: popupStyle.popup__container__open,
+    [PopupVisibleStateEnum.closing]: popupStyle.popup__container__closing,
+};
+
 type PopupPropsType = {
     children: ReactNode;
     isOpen: boolean;
+    zIndex?: number;
 };
-
-enum VisibleStateEnum {
-    closed = 'closed',
-    closing = 'closing',
-    open = 'open',
-    opening = 'opening',
-}
 
 export function Popup(props: PopupPropsType): JSX.Element | null {
     const transitionTime = 1e3;
-    const {isOpen = false, children} = props;
+    const {isOpen = false, children, zIndex = 1000} = props;
 
     const [isSelfOpen, setIsSelfOpen] = useState<boolean>(false);
     const [isMounted, setIsMounted] = useState<boolean>(false);
     const timeOutRef = useRef<NodeJS.Timeout | number>(Number.NaN);
 
-    const visibleState: VisibleStateEnum = (() => {
+    const visibleState: PopupVisibleStateEnum = (() => {
         if (isOpen === isSelfOpen) {
-            return isSelfOpen ? VisibleStateEnum.open : VisibleStateEnum.closed;
+            return isSelfOpen ? PopupVisibleStateEnum.open : PopupVisibleStateEnum.closed;
         }
 
-        return isOpen ? VisibleStateEnum.opening : VisibleStateEnum.closing;
+        return isOpen ? PopupVisibleStateEnum.opening : PopupVisibleStateEnum.closing;
     })();
 
     const infoState = [
@@ -60,7 +71,7 @@ export function Popup(props: PopupPropsType): JSX.Element | null {
         }, transitionTime);
     }, [isOpen, isSelfOpen, timeOutRef]);
 
-    if (visibleState === VisibleStateEnum.closed) {
+    if (visibleState === PopupVisibleStateEnum.closed) {
         return null;
     }
 
@@ -68,10 +79,18 @@ export function Popup(props: PopupPropsType): JSX.Element | null {
         return null;
     }
 
+    const style = {zIndex};
+
+    const fadeClassName = `${popupStyle.popup__fade} ${fadeClassNameMap[visibleState]}`;
+    const containerClassName = `${popupStyle.popup__container} ${containerClassNameMap[visibleState]}`;
+
     return createPortal(
-        <div>
-            {children} {visibleState}
-        </div>,
+        <>
+            <div className={fadeClassName} style={style} />
+            <div className={containerClassName} style={style}>
+                {children} {visibleState}
+            </div>
+        </>,
         document.body
     );
 }
