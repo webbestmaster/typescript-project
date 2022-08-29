39a6c06d2fa2d4ffdf3ba7c87a4cd74899e607e2
Dmitry Turovtsov <dmitry.turovtsov@consultant.volvo.com>
2022-08-29 17:27:39 +0200

    update deps

---
+++ .storybook/main.js
@@ -0,0 +1,34 @@
+const styleLoader = {
+    loader: 'style-loader',
+    options: {attributes: {class: 'my-css-module'}},
+};
+
+module.exports = {
+    'stories': ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+    'addons': ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+    'framework': '@storybook/react',
+    'core': {
+        'builder': 'webpack5',
+    },
+    webpackFinal: async (config, {configType}) => {
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: [
+                styleLoader,
+                'css-modules-typescript-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: false,
+                        modules: {
+                            localIdentName: '[local]----[hash:6]',
+                        },
+                    },
+                },
+                {loader: 'sass-loader', options: {sourceMap: false}},
+            ],
+        });
+
+        return config;
+    },
+};
---
+++ stories/Button.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import buttonStyle from './button.scss';
+
+interface ButtonProps {
+    /**
+     * What background color to use
+     */
+    backgroundColor?: string;
+    /**
+     * Button contents
+     */
+    label: string;
+    /**
+     * Optional click handler
+     */
+    onClick?: () => void;
+    /**
+     * Is this the principal call to action on the page?
+     */
+    primary?: boolean;
+    /**
+     * How large should the button be?
+     */
+    size?: 'large' | 'medium' | 'small';
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export function Button({primary = false, size = 'medium', backgroundColor, label, ...props}: ButtonProps) {
+    const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+    return (
+        <button
+            className={[buttonStyle['storybook-button'], buttonStyle[`storybook-button--${size}`], mode].join(' ')}
+            style={{backgroundColor}}
+            type="button"
+            {...props}
+        >
+            {label}
+        </button>
+    );
+}
