a6348e5df2435450a123815dd66e5e30c7576bab
Dmitry Turovtsov <dmitry.turovtsov@consultant.volvo.com>
2022-12-15 11:04:04 +0100

    small refactoring

--- test-e2e/config/jest.config.js
+++ test-e2e/config/jest.config.js
@@ -1,9 +1,14 @@
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = {
-    modulePathIgnorePatterns: ['<rootDir>/tsc-check/'],
-    preset: 'ts-jest',
-    rootDir: '../../',
+    bail: true, // stop after first failing test
+    errorOnDeprecated: true,
+    injectGlobals: true,
     // setupFilesAfterEnv: ['./test-unit/config/setup-jest.ts'],
     // testEnvironment: 'jsdom', // @testing-library/jest-dom
+    modulePathIgnorePatterns: ['<rootDir>/tsc-check/'],
+    passWithNoTests: true,
+    preset: 'ts-jest',
+    rootDir: '../../',
+    silent: true,
     testTimeout: 10e3,
 };
--- test-unit/config/jest.config.js
+++ test-unit/config/jest.config.js
@@ -1,12 +1,18 @@
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = {
+    bail: true, // stop after first failing test
+    collectCoverage: true,
+    errorOnDeprecated: true,
+    injectGlobals: true,
     moduleNameMapper: {
         '^\\S+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     },
     modulePathIgnorePatterns: ['<rootDir>/tsc-check/'],
+    passWithNoTests: true,
     preset: 'ts-jest',
     rootDir: '../../',
     setupFilesAfterEnv: ['./test-unit/config/setup-jest.ts'],
+    silent: true,
     testEnvironment: 'jsdom', // @testing-library/jest-dom
     testTimeout: 10e3,
 };
