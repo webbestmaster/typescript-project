722e4488f908ea68bb07fe5c3b56fe1bcdc7b790
dim <web.best.master@gmail.com>
2024-02-23 22:27:40 +0100

    img alt

--- www/client-component/auth/login-form/login-form.tsx
+++ www/client-component/auth/login-form/login-form.tsx
@@ -1,13 +1,17 @@
-import {useCallback, type SyntheticEvent} from "react";
+import {useCallback, useState, type SyntheticEvent} from "react";
 
 import {useUserContext} from "../../../provider/user/user-context";
 import {useMakeExecutableState} from "../../../util/function";
-import {logoutUser} from "../../../service/auth/auth-api";
+import {loginUser, logoutUser} from "../../../service/auth/auth-api";
 import type {LoginResponseType} from "../../../service/auth/auth-type";
 import {throwError} from "../../../util/error";
+import {apiUrl} from "../../../../server/const";
 
 export function LoginForm(): JSX.Element {
     const userContext = useUserContext();
+    const [login, setLogin] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const loginHook = useMakeExecutableState<Parameters<typeof loginUser>, LoginResponseType>(loginUser);
     const logoutHook = useMakeExecutableState<Parameters<typeof logoutUser>, LoginResponseType>(logoutUser);
 
     const onHandleLogout = useCallback(
@@ -27,10 +31,26 @@ export function LoginForm(): JSX.Element {
         [logoutHook, userContext]
     );
 
+    const onSubmit = useCallback(
+        (evt: SyntheticEvent<HTMLFormElement>) => {
+            evt.preventDefault();
+
+            loginHook
+                .execute(login, password)
+                .then((loginResponse: LoginResponseType) => {
+                    const {user} = loginResponse;
+
+                    userContext.setUser(user);
+                })
+                .catch(throwError);
+        },
+        [login, password, loginHook, userContext]
+    );
+
     if (userContext.user.login !== "") {
         return (
             <div>
-                <p>you are already logged in, the login is &quot;{userContext.user.login}&quot;</p>
+                <p>you are logged in, the login is &quot;{userContext.user.login}&quot;</p>
                 <button onClick={onHandleLogout} type="button">
                     logout
                 </button>
@@ -39,12 +59,35 @@ export function LoginForm(): JSX.Element {
     }
 
     return (
-        <form>
-            {JSON.stringify(userContext.user)}
-            <input placeholder="login" type="text" />
-            <hr />
-            <input placeholder="password" type="password" />
-            <hr />
+        <form action={apiUrl.login} onSubmit={onSubmit}>
+            <pre>{JSON.stringify(userContext, null, 4)}</pre>
+            <pre>{JSON.stringify(loginHook, null, 4)}</pre>
+
+            <label>
+                <p>login</p>
+                <input
+                    onInput={(evt: SyntheticEvent<HTMLInputElement>): undefined => {
+                        setLogin(evt.currentTarget.value);
+                    }}
+                    placeholder="login"
+                    type="text"
+                />
+            </label>
+            <label>
+                <p>password</p>
+                <input
+                    onInput={(evt: SyntheticEvent<HTMLInputElement>): undefined => {
+                        setPassword(evt.currentTarget.value);
+                    }}
+                    placeholder="password"
+                    type="text"
+                />
+            </label>
+
+            {loginHook.error ? <h3>ERROR login</h3> : "???"}
+
+            <br />
+
             <button type="submit">submit</button>
         </form>
     );
