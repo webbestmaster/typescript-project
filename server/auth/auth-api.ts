108e2f91edba7511bfa11a62e71db8e06e89333c
dim <web.best.master@gmail.com>
2024-02-24 20:49:37 +0100

    register - end

--- server/auth/auth-api.ts
+++ server/auth/auth-api.ts
@@ -6,6 +6,8 @@ import {getSha256HashServer} from "../util/string";
 import {UserRoleEnum} from "../../www/provider/user/user-context-type";
 import {mainResponseHeader} from "../const";
 
+import {getRandomString} from "../../www/util/string";
+
 import {authCrud} from "./auth";
 import {cookieFieldUserId} from "./auth-const";
 
@@ -87,11 +89,7 @@ export async function getAutoAuthLogin(request: FastifyRequest, reply: FastifyRe
     return loginResponse;
 }
 
-// eslint-disable-next-line @typescript-eslint/require-await
-export async function postAuthLogout(
-    request: FastifyRequest<{Body?: string}>,
-    reply: FastifyReply
-): Promise<LoginResponseType> {
+export function postAuthLogout(request: FastifyRequest<{Body?: string}>, reply: FastifyReply): LoginResponseType {
     const {session} = request;
 
     session.set(cookieFieldUserId, "");
@@ -100,6 +98,64 @@ export async function postAuthLogout(
         user: {
             id: "",
             login: "",
+            role: UserRoleEnum.user,
+        },
+    };
+
+    reply.header(...mainResponseHeader);
+
+    return loginResponse;
+}
+
+export async function postAuthRegister(
+    request: FastifyRequest<{Body?: string}>,
+    reply: FastifyReply
+): Promise<LoginResponseType> {
+    const {body, session} = request;
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const parsedData: Record<string, unknown> = JSON.parse(String(body ?? "{}"));
+
+    const {login, password, email} = parsedData;
+
+    if (typeof email !== "string" || typeof login !== "string" || typeof password !== "string") {
+        reply.code(400);
+
+        throw new Error("Login, email or password is not define.");
+    }
+
+    const userInDataBase = await authCrud.findOne({login});
+
+    if (userInDataBase) {
+        reply.code(400);
+
+        const registerErrorResponse: LoginResponseType = {
+            user: {
+                id: "",
+                login: "",
+                role: UserRoleEnum.user,
+            },
+        };
+
+        return registerErrorResponse;
+    }
+
+    const createdUserId: string = getRandomString();
+
+    await authCrud.createOne({
+        id: createdUserId,
+        login,
+        password: getSha256HashServer(password),
+        role: UserRoleEnum.user,
+    });
+
+    session.set(cookieFieldUserId, createdUserId);
+    session.options({maxAge: 1000 * 60 * 60});
+
+    const loginResponse: LoginResponseType = {
+        user: {
+            id: createdUserId,
+            login,
             role: UserRoleEnum.user,
         },
     };
